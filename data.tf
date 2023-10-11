57775f24527d5026eac2cf4e0d3fa5b74f86c625
Yashwanth Kotha <yashwanth.kotha@onpassive.org>
2023-10-11 09:38:08 +0530

    terraformcode

---
+++ data.tf
@@ -0,0 +1,10 @@
+data "aws_ami" "ec2" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+
+}
+
+output "ami_id" {
+    value = data.aws_ami.ec2.image_id
+}
