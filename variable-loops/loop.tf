c2959ef6282fea61518ebfaabb524cc79e361a8e
Yashwanth Kotha <yashwanth.kotha@onpassive.org>
2023-10-11 15:28:41 +0530

    terraformcode

--- variable-loops/loop.tf
+++ variable-loops/loop.tf
@@ -7,3 +7,14 @@ resource "null_resource" "name" {
   count = var.d1
 
 }
+
+
+variable "d2" {
+  default = ["apple", 5, True]
+  
+}
+
+resource "null_resource" "name" {
+  count = length(var.d2)
+
+}
