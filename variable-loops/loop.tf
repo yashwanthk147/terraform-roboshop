489816746aef69b33db1c615fed424aef0f42b02
Yashwanth Kotha <yashwanth.kotha@onpassive.org>
2023-10-11 15:22:43 +0530

    terraformcode

--- variable-loops/loop.tf
+++ variable-loops/loop.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+
+
 variable "d1" {
   default = 5
   
