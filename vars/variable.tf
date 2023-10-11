60e7efe761ff8db3c483f359748e4295d9467799
Yashwanth Kotha <yashwanth.kotha@onpassive.org>
2023-10-11 10:12:34 +0530

    terraformcode

--- vars/variable.tf
+++ vars/variable.tf
@@ -22,4 +22,38 @@ output "var_cli" {
 }
 
 #terraform apply -var cli="HELLO SHELL SCRIPT"
+#terraform apply -var-file cli="HELLO SHELL SCRIPT"
+#terraform apply -var-file=dev.tfvars
 #export TF_VAR_cli="HELLO FROM SHELL ENV AVRIABLE"
+
+
+#precedence  -var is higher precedence and -var-file is next precedence
+#first it checks for command line variabales and then it goes for .auto.tfvars, terraform.tfvars, shell env, and then it ask input
+
+
+#strings should always cotted in "", where as numbers and booleans are not
+
+//variable "sample5" {
+//  default = "Hello"
+//}
+//
+//variable "sample6" {
+//  default = [
+//    "Hello",
+//    1000,
+//    true,
+//    "World"
+//  ]
+//}
+//
+//variable "sample7" {
+//  default = {
+//    string  = "Hello",
+//    number  = 100,
+//    boolean = true
+//  }
+//}
+//
+//output "types" {
+//  value = "Variable sample5 - ${var.sample5}, First value in list - ${var.sample6[0]}, Boolean Value of Map = ${var.sample7["boolean"]}"
+//}
