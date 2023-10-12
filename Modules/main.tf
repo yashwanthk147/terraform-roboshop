82ab99f06b00b9c8c6dd91a356530bec608e0b40
Yashwanth Kotha <yashwanth.kotha@onpassive.org>
2023-10-12 10:45:38 +0530

    terraformcode

--- Modules/main.tf
+++ Modules/main.tf
@@ -2,7 +2,7 @@ module "ec2" {
     for_each = var.instance
     source = "./ec2"
     component = each.value["name"]
-    instance_type = each.value["component"]
+    instance_type = each.value["type"]
   
 }
 
