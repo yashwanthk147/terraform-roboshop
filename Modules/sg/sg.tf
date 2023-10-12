0db12d1b515718b50e415a6e41b8f503c5aef6ff
Yashwanth Kotha <yashwanth.kotha@onpassive.org>
2023-10-12 12:00:45 +0530

    terraformcode

--- Modules/sg/sg.tf
+++ Modules/sg/sg.tf
@@ -1,7 +1,6 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
 
   ingress {
     description      = "TLS from VPC"
