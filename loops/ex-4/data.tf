e3a3e489bbabfbd9d320412e28159d685cfd844c
Yashwanth Kotha <yashwanth.kotha@onpassive.org>
2023-10-12 09:39:29 +0530

    terraformcode

--- loops/ex-4/data.tf
+++ loops/ex-4/data.tf
@@ -9,7 +9,7 @@ data "aws_ami" "ami" {
 resource "aws_instance" "ec2" {
     count                    = 5
     ami                      = data.aws_ami.ami.image_id
-    instance_type            = var.instance_type[count.index]
+    instance_type            = var.instance_type[0]
     vpc_security_group_ids   = ["sg-05a0128acebb0e8b2"]
     tags = {
       Name             = "cart-${count.index}"
