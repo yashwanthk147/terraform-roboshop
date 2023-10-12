ce11e3abfed1dc438c2eef6a539f4f9c94687b55
Yashwanth Kotha <yashwanth.kotha@onpassive.org>
2023-10-12 10:13:25 +0530

    terraformcode

--- conditions/resource.tf
+++ conditions/resource.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
     count = var.create_intsance == "true" ? 1 : 0
     ami = "ami-03265a0778a880afb"
-    instance_type = var.instance_type == "" ? t3.micro : var.instance_type
+    instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
 
   
 }
